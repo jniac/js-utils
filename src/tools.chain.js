0276ec1190c601c9751da91e0a3bdde9ebcb1b2b
jniac <joseph.merdrignac@gmail.com>
2017-11-14 14:06:20 +0100

    introducing Chain

---
+++ src/tools.chain.js
@@ -0,0 +1,140 @@
+class Link {
+
+	constructor(key, value) {
+
+		this.key = key
+		this.value = value
+
+	}
+
+	toString() {
+
+		return `Link { value: ${this.value} }`
+
+	}
+
+	// constructor(value) {
+
+	// 	Object.defineProperty(this, '__value', { 
+
+	// 		writable: true,
+
+	// 	})
+
+	// 	this.setValue(value)
+
+	// }
+
+	// setValue(value) {
+
+	// 	if (this.__value === value)
+	// 		return this
+
+	// 	Object.defineProperty(this, '__value', { value })
+
+	// }
+
+	// get value () { return this.__value }
+
+	// set value(value) { this.setValue(value) }
+
+}
+
+const clamp = x => x < 0 ? 0 : x > 1 ? 1 : x
+
+const relations = {
+
+	'*': (a, b) => a * b,
+	'+': (a, b) => a + b,
+	'&&': (a, b) => a && b,
+	'||': (a, b) => a || b,
+	'clamp(*)': (a, b) => clamp(a * b),
+
+}
+
+export class Chain {
+
+	constructor(relation = '*', initialValue = 1) {
+
+		Object.defineProperties(this, {
+
+			links: { value: [] },
+
+		})
+
+		this.relation = relation
+		this.initialValue = initialValue
+
+		this.count = 0
+
+	}
+
+	getValue() {
+
+		let value = 1
+
+		for (let link of this.links)
+			value = relations[this.relation](value, link.value)
+
+		return value
+
+	}
+
+	get value() { return this.getValue() }
+
+	searchLink(key) {
+
+		for (let link of this.links)
+			if (link.key === key)
+				return link
+
+		return null
+
+	}
+
+	createLink(key, value) {
+
+		let link = this.searchLink(key)
+
+		if (link)
+			throw `a Link already exist for ${String(key)}`
+
+		link = new Link(key, value)
+
+		this.links.push(link)
+
+		this.count++
+
+		return link
+
+	}
+
+	getLink(key, { createIfNull = true, value = null } = {}) {
+
+		let link = this.searchLink(key)
+
+		if (!link && createIfNull)
+			link = this.createLink(key)
+
+		if (link && value)
+			link.value = value
+
+		return link
+
+	}
+
+	link(key, options) {
+
+		this.getLink(key, options)
+
+		return this
+
+	}
+
+	toString() {
+
+		return `Chain { relation: ${this.relation}, count: ${this.count}, value: ${this.value} }`
+
+	}
+
+}
---
+++ src/tools.js
@@ -0,0 +1,2 @@
+import { Chain } from './tools.chain.js'
+export { Chain }
