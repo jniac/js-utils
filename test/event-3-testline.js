016e51de04b6c6b8eadc986d803124fe750cf823
jniac <joseph.merdrignac@gmail.com>
2018-01-06 00:02:51 +0100

    testline

--- test/event-3-testline.js
+++ test/event-3-testline.js
@@ -8,7 +8,83 @@ eventjs.dispatchEvent(object, 'foo')
 
 // log.break
 
-log.assert(`must be written "foo" above`, () => log.last === 'foo')
+log.assert(`must be written "foo" above`, log.last === 'foo')
+
+
+
+
+
+
+
+// TEST - Usage
+
+let count = 0
+
+let logEvent = event => log(count++, event.type)
+
+let foo = { }
+let bar = new eventjs.EventDispatcher()
+let quz = { }
+eventjs.implementEventDispatcher(quz)
+
+eventjs.on(foo, 'foo!', logEvent)
+bar.on('bar!!', logEvent)
+quz.on('quz!!!', logEvent)
+
+eventjs.dispatchEvent(foo, 'foo!')
+bar.dispatchEvent('bar!!')
+quz.dispatchEvent('quz!!!')
+
+// log.break
+
+log.assert('foo, bar & quz have logged', log.getLog(-3) === '0 foo!' && log.getLog(-2) === '1 bar!!' && log.getLog(-1) === '2 quz!!!')
+
+// log.break
+
+log.info(`event & listener dissociation
+remove ONE listener:
+off(eventType, eventListener)`)
+
+// log.break
+
+eventjs.off(foo, 'foo!', logEvent)
+bar.off('bar!!', logEvent)
+quz.off('quz!!!', logEvent)
+
+eventjs.dispatchEvent(foo, 'foo!')
+bar.dispatchEvent('bar!!')
+quz.dispatchEvent('quz!!!')
+
+log.assert(`nothing has logged`, log.getLog(-3) === '0 foo!' && log.getLog(-2) === '1 bar!!' && log.getLog(-1) === '2 quz!!!')
+
+// log.break
+
+eventjs.on(foo, 'foo!', logEvent)
+bar.on('bar!!', logEvent)
+quz.on('quz!!!', logEvent)
+
+eventjs.dispatchEvent(foo, 'foo!')
+bar.dispatchEvent('bar!!')
+quz.dispatchEvent('quz!!!')
+
+// log.break
+
+log.assert('foo, bar & quz have come back', log.getLog(-3) === '3 foo!' && log.getLog(-2) === '4 bar!!' && log.getLog(-1) === '5 quz!!!')
+
+// log.break
+
+log.info(`event & listener dissociation
+remove ALL listener:
+off(eventType, eventListener)`)
+
+// log.break
+eventjs.off(foo, 'foo!')
+bar.off('bar!!')
+quz.off('quz!!!')
+
+log.assert('nothing has logged', log.getLog(-3) === '3 foo!' && log.getLog(-2) === '4 bar!!' && log.getLog(-1) === '5 quz!!!')
+
+
 
 
 
@@ -18,7 +94,6 @@ log.assert(`must be written "foo" above`, () => log.last === 'foo')
 
 // TEST - Priority
 
-
 let object = {}
 
 eventjs.on(object, 'foo', event => log('listener 1:', event.type))
@@ -27,7 +102,7 @@ eventjs.dispatchEvent(object, 'foo')
 
 // log.break
 
-log.assert(`2 listeners should have log ("listener 1: foo", "listener 2: foo")`, () => log.getLog(-2) === 'listener 1: foo' && log.getLog(-1) === 'listener 2: foo')
+log.assert(`2 listeners should have log ("listener 1: foo", "listener 2: foo")`, log.getLog(-2) === 'listener 1: foo' && log.getLog(-1) === 'listener 2: foo')
 
 
 
@@ -38,7 +113,7 @@ eventjs.dispatchEvent(object, 'foo')
 
 // log.break
 
-log.assert(`listener 3 should be first to log`, () => /listener 3/.test(log.getLog(-3)))
+log.assert(`listener 3 should be first to log`, /listener 3/.test(log.getLog(-3)))
 
 
 
@@ -60,5 +135,38 @@ eventjs.dispatchEvent(object, 'foo')
 
 // log.break
 
-log.assert(`order should be '4,6,3,5,1,2'`, () => log.getLogs(-6).map(v => v.slice(9, 10)).join() === '4,6,3,5,1,2')
+log.assert(`order should be '4,6,3,5,1,2'`, log.getLogs(-6).map(v => v.slice(9, 10)).join() === '4,6,3,5,1,2')
+
+
+
+
+
+
+
+// TEST - thisArg, args
+
+let foo = new eventjs.EventDispatcher()
+
+class Show {
+
+	constructor() {
+
+		foo.on('BAR', this.onBar, { thisArg: this, args: ['me the way to the next', 'whisky'] })
+
+	}
+
+	onBar(event, lyrics, lyrics2) {
+
+		log(this.constructor.name, lyrics, lyrics2, event.type)
+
+	}
+
+}
+
+new Show()
+
+foo.dispatchEvent('BAR')
+
+log.assert('should be logged: "Show me the way to the next whisky BAR"', log.last === 'Show me the way to the next whisky BAR')
+
 
