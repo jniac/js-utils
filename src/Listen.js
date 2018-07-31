5df762fd78e2c6e7f8964ab76077df3ccf4dd03c
jniac <joseph.merdrignac@gmail.com>
2018-07-31 13:59:01 +0200

    add Listen.js (stunning powerful Listener Manager)

---
+++ src/Listen.js
@@ -0,0 +1,293 @@
+
+let register = (() => {
+
+    let objectMap = new WeakMap()
+
+    let stringMap = new Map()
+
+    window.stringMap = stringMap
+
+    return {
+
+        get: target => (
+
+			target && typeof target === 'object'
+
+				? objectMap.get(target)
+
+				: stringMap.get(target)
+
+		),
+
+        set: (target, value) => {
+
+			target && typeof target === 'object'
+
+				? objectMap.set(target, value)
+
+				: stringMap.set(target, value)
+
+        },
+
+        delete: (target) => {
+
+			target && typeof target === 'object'
+
+                ? objectMap.delete(target)
+
+                : stringMap.delete(target)
+
+        },
+
+    }
+
+})()
+
+const SPLIT = /\s*,\s*|\s+/
+
+const filterMatches = (filter, type) => {
+
+	if (filter === '*') {
+
+		return true
+
+	}
+
+	if (typeof filter === 'string' && SPLIT.test(filter)) {
+
+		return filter.split(SPLIT).includes(type)
+
+	}
+
+	if (filter instanceof RegExp) {
+
+		return filter.test(type)
+
+	}
+
+	if (Array.isArray(filter)) {
+
+		return filter.some(value => filterMatches(value, type))
+
+	}
+
+	return type === filter
+
+}
+
+const getFilterMatch = (filter) => {
+
+	if (filter === '*') {
+
+		return () => true
+
+	}
+
+	if (typeof filter === 'string' && SPLIT.test(filter)) {
+
+		let array = filter.split(SPLIT)
+
+		return type => array.includes(type)
+
+	}
+
+	if (filter instanceof RegExp) {
+
+		return type => filter.test(type)
+
+	}
+
+	if (Array.isArray(filter)) {
+
+		return type => filter.some(filter => filterMatches(filter, type))
+
+	}
+
+	return type => type === filter
+
+}
+
+const clear = (target) => {
+
+	register.delete(target)
+
+}
+
+const ensure = (target) => {
+
+	let array = register.get(target)
+
+	if (!array) {
+
+		register.set(target, array = [])
+
+	}
+
+	return array
+
+}
+
+const EMPTY_ARRAY = []
+let listenerCount = 0
+
+const add = (target, filter, callback, { thisArg = null, args = EMPTY_ARRAY, priority = 0, key = null } = {}) => {
+
+	let match = getFilterMatch(filter)
+
+	if (Array.isArray(callback)) {
+
+		thisArg = callback[0]
+		callback = thisArg[callback[1]]
+
+	}
+
+	let listener = { filter, match, callback, thisArg, args, priority, key }
+
+    Object.defineProperty(listener, 'uid', { value: listenerCount++, enumerable: true })
+
+	let array = ensure(target)
+
+	let index = 0
+
+	while (index < array.length) {
+
+		if (priority > array[index].priority)
+			break
+
+		index++
+
+	}
+
+	array.splice(index, 0, listener)
+
+}
+
+const ALL = Symbol('Listen.ALL')
+
+const remove = (target, filter = '*', callback = ALL, { uid = -1, thisArg = null, key = null } = {}) => {
+
+	let array = register.get(target)
+
+	if (!array)
+		return
+
+	if (Array.isArray(callback)) {
+
+		thisArg = callback[0]
+		callback = thisArg[callback[1]]
+
+	}
+
+	for (let i = 0, { length } = array; i < length; i++) {
+
+		let listener = array[i]
+
+		let match
+
+        if (uid === listener.uid) {
+
+            match = true
+
+        } else {
+
+            match = filter === '*' || listener.filter === filter
+
+    		if (match && callback !== ALL)
+    			match = callback === listener.callback
+
+    		if (match && thisArg)
+    			match = thisArg === listener.thisArg
+
+    		if (match && key)
+    			match = key === listener.key
+
+        }
+
+		if (match) {
+
+            listener.killed = true
+			array.splice(i, 1)
+			i--
+			length--
+
+		}
+
+	}
+
+    if (array.length === 0) {
+        
+        register.delete(target)
+
+    }
+
+}
+
+let currentType, currentCancel
+
+const current = Object.defineProperties({}, {
+
+	type: { get: () => currentType, enumerable: true },
+	cancel: { get: () => currentCancel, enumerable: true },
+
+})
+
+const KILL = Symbol('Listen.KILL')
+
+const call = (target, type, ...callArgs) => {
+
+	if (SPLIT.test(type)) {
+
+		type = type.split(SPLIT)
+
+	}
+
+	if (Array.isArray(type)) {
+
+		for (let _type of type)
+			call(target, _type, ...callArgs)
+
+		return
+
+	}
+
+	let array = register.get(target)
+
+	if (!array)
+		return
+
+    // do not forget to copy the array
+	array = [...array]
+
+	let canceled = false
+	let cancel = () => canceled = true
+
+	for (let listener of array) {
+
+		if (canceled)
+			break
+
+        let { match, callback, thisArg, args } = listener
+
+        currentType = type
+        currentCancel = cancel
+
+        let doesMatch = match(type)
+        let killed = doesMatch && callback.apply(thisArg, [...args, ...callArgs]) === KILL
+
+        if (doesMatch && killed)
+            remove(target, null, null, { uid: listener.uid })
+
+	}
+
+}
+
+export default {
+
+	add,
+	remove,
+	call,
+	current,
+
+	debug: (target) => register.get(target),
+
+}
---
+++ test/listen-test.js
@@ -0,0 +1,218 @@
+
+import Listen from '../src/Listen.js'
+
+console.logBreak = () => console.log('- '.repeat(1 + 42/2))
+console.logTitle = (...titles) => {
+
+    let n = 41
+
+    titles = titles.map((title) => {
+
+        let n1 = Math.floor((n - title.length) / 2)
+        let n2 = n - title.length - n1
+
+        return `-${' '.repeat(n1) + title + ' '.repeat(n2)}-`
+
+    }).join('\n')
+
+    let str =
+        `-${' '.repeat(n)}-` + '\n' +
+        titles + '\n' +
+        `-${' '.repeat(n)}-`
+
+    console.log(str)
+
+}
+
+
+
+Listen.add('FOO', 'BAR', event => console.log(event))
+Listen.call('FOO', 'BAR', 'hello')
+
+console.logBreak()
+
+// multiple listener
+let arr = ['BAZ','QUX']
+Listen.add('FOO', 'BAZ,QUX', event => console.log('string', Listen.current.type, event))
+Listen.add('FOO', arr, event => console.log('array', Listen.current.type, event))
+Listen.add('FOO', '*', () => console.log(`it's a trap (*)`))
+Listen.add('FOO', /baz|qux/i, () => console.log(`re has you (/baz|qux/i)`))
+
+Listen.call('FOO', 'BAZ', 'hello baz')
+
+console.logBreak()
+
+// multiple call
+Listen.call('FOO', 'BAZ,QUX', 'hello both')
+
+console.logBreak()
+
+Listen.remove('FOO', 'BAZ,QUX')
+Listen.call('FOO', 'BAZ,QUX', 'no more string [BAZ,QUX] listener here')
+
+console.logBreak()
+
+Listen.call('FOO', 'BAR', 'hello')
+
+console.logBreak()
+
+arr.push('BAR')
+Listen.call('FOO', 'BAR', 'hello')
+
+console.logBreak()
+
+Listen.remove('FOO', arr)
+Listen.call('FOO', 'BAR', 'hello')
+
+console.logBreak()
+
+Listen.remove('FOO', '*')
+Listen.call('FOO', 'BAZ,QUX', 'no more listener here')
+Listen.call('FOO', 'BAR', 'no more listener here')
+
+
+
+
+console.logBreak()
+console.logTitle('cancel(), priority & key')
+
+Listen.add(window, 'an-event', (...args) => console.log('listener 1', Listen.current.type, ...args))
+Listen.add(window, 'an-event', (...args) => console.log('listener 2', Listen.current.type, ...args))
+Listen.add(window, 'an-event', (...args) => console.log('listener 3', Listen.current.type, ...args))
+Listen.call(window, 'an-event', 'with', 'some', 'args')
+
+console.logBreak()
+
+let A_UNIQUE_KEY = Symbol('A_UNIQUE_KEY')
+
+Listen.add(window, 'an-event', (...args) => {
+
+	console.log('listener ZERO!!!', Listen.current.type, ...args)
+	Listen.current.cancel()
+
+}, { priority: 1, key: A_UNIQUE_KEY })
+
+Listen.call(window, 'an-event', 'that will', 'be canceled', 'by a first listener!!!!')
+
+console.logBreak()
+
+Listen.remove(window, 'an-event', null, { key: A_UNIQUE_KEY })
+Listen.call(window, 'an-event', 'with', 'some', 'args')
+
+
+
+
+console.logBreak()
+console.logTitle('class/instances case', '[this, methodName]')
+
+let FooCount = 0
+class Foo {
+
+	constructor() {
+
+		this.uid = FooCount++
+		Listen.add(Foo, 'bar', [this, 'print'])
+
+	}
+
+	unsubscribe() {
+
+		Listen.remove(Foo, 'bar', [this, 'print'])
+
+	}
+
+	print() {
+
+		console.log(`Foo#${this.uid}{} react to "${Listen.current.type}" event`)
+
+	}
+
+}
+
+let fooz = [new Foo(), new Foo(), new Foo()]
+
+Listen.call(Foo, 'bar')
+
+console.logBreak()
+
+Listen.remove(Foo, 'bar', fooz[1].print)
+// Listen.remove(Foo, 'bar', Foo.prototype.print)
+Listen.call(Foo, 'bar')
+console.log(`...
+(all callbacks have been removed
+because the callback is a prototype member
+(shared between all instances))
+...`)
+
+console.logBreak()
+
+fooz = [new Foo(), new Foo(), new Foo(), new Foo()]
+
+console.log(`Foo, 'bar'`)
+Listen.call(Foo, 'bar')
+
+console.logBreak()
+
+Listen.remove(Foo, 'bar', [fooz[1], 'print'])
+
+console.log(`Foo, 'bar'`)
+Listen.call(Foo, 'bar')
+
+console.logBreak()
+
+fooz[0].unsubscribe()
+
+console.log(`Foo, 'bar'`)
+Listen.call(Foo, 'bar')
+
+console.logBreak()
+
+Listen.remove(Foo, 'bar', Listen.ALL)
+
+console.log(`Foo, 'bar'`)
+Listen.call(Foo, 'bar')
+
+console.logBreak()
+
+
+
+console.logTitle('add() or remove()', 'from inside a call', 'does NOT corrupt', 'the current call')
+
+Listen.add(null, 'START', (message) => {
+
+    console.log(`fill start [${message}]`)
+
+    for (let i = 0; i < 4; i++)
+        Listen.add(null, 'START', (message) => console.log(`starter #${i} [${message}]`))
+
+    Listen.add(null, 'START', (message) => console.log(`starter ZERO! [${message}]`), { priority:1  })
+
+    Listen.add(null, 'START', (message) => {
+
+        console.log('clear start')
+
+        Listen.remove(null, 'START', Listen.ALL)
+
+    })
+
+    for (let i = 4; i < 8; i++)
+        Listen.add(null, 'START', (message) => console.log(`starter #${i} [${message}]`))
+
+    return Listen.KILL
+
+})
+
+console.log('first:')
+Listen.call(null, 'START', 'first')
+console.logBreak()
+
+console.log('second:')
+Listen.call(null, 'START', 'second')
+console.logBreak()
+
+console.log('muted:')
+Listen.call(null, 'START', 'muted')
+console.logBreak()
+
+
+export { Listen }
